7fe6eb0c3931c77e1801bea1fd1865353f362a54
Juliana Spector <julianaspector@gmail.com>
2020-01-24 11:40:17 -0800

    Add files via upload

---
+++ CDEC_Retrieve_example.R
@@ -0,0 +1,15 @@
+library(CDECRetrieve)
+
+MOK_available <- cdec_datasets(station="MOK")
+
+MOK_data <- cdec_query(station="MOK",
+                       sensor_num=25,
+                       start_date="2018-10-01",
+                       end_date="2019-09-30",
+                       dur_code = "E")
+
+ggplot(MOK_data) + 
+  geom_point(aes(x=datetime, y=parameter_value)) + 
+  ylab(expression("Temperature " (degree*F))) + 
+  xlab("Date") +
+  ggtitle("Temperature for Mokelumne River at San Joaquin River during WY 2019")
