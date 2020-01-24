7dc40d2a9000db89470d2b8ae123d0c95946312e
Juliana Spector <julianaspector@gmail.com>
2020-01-24 09:48:07 -0800

    Add files via upload

---
+++ dataRetrieval_example.R
@@ -0,0 +1,19 @@
+library(dataRetrieval)
+library(ggplot2)
+
+siteNumber <- "11303000" # Stanislaus River at Ripon
+service <- "iv"
+parameterCd <- "00060"
+
+DataAvailable <- whatNWISdata(siteNumber=siteNumber,
+                              service=service,
+                              parameterCd=parameterCd)
+
+startDate <- "2012-10-01"
+endDate <- "2013-09-30"
+data <- readNWISuv(siteNumber, parameterCd, startDate, endDate)
+data <- renameNWISColumns(data)
+
+
+ggplot(data) + geom_point(aes(x=data$dateTime, y=data$Flow_Inst)) + ylab("Discharge (cfs)") + xlab("Date")
+
